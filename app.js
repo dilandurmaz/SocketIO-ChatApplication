26df6ec0701d1993c81dd809ac8b16cef8ee21ee
dilandurmaz <45289986+dilandurmaz@users.noreply.github.com>
2020-11-23 17:28:16 +0300

    mongodbconnected

--- app.js
+++ app.js
@@ -28,11 +28,10 @@ io.on('connection',(socket)=>{
     
     socket.on('joinRoom', (data) => {
         socket.join(data.roomName,() => {
-        
             io.to(data.roomName).emit('newJoin' ,{count : getOnlineCount(io,data),message :`  ${data.userName}  joined to ${data.roomName} room <br> `});
             socket.emit('joinedRoom');
-            const rooms = Object.keys(socket.rooms);
-            console.log(rooms);
+            // const rooms = Object.keys(socket.rooms);
+            // console.log(rooms);
            
          
         });
