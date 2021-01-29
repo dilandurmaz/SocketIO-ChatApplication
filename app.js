02b8bb20e51245790b3ae296c2f99e51a3a4b890
dilandurmaz <45289986+dilandurmaz@users.noreply.github.com>
2021-01-30 01:39:27 +0300

    user

--- app.js
+++ app.js
@@ -3,86 +3,93 @@ const app = express();
 const server = require('http').createServer(app);
 const socketio = require('socket.io');
 const mongoose = require('mongoose');
-server.listen(3000,()=>{
+server.listen(3000, () => {
     console.log("listening to 3000 port");
 });
 
 // MONGODB CONNECT
-mongoose.connect('mongodb://localhost/chat',(err)=>{
-   if(err){
-       console.log(err);
-   }else{
-       console.log('Connected to mongodb!');
-   }
+mongoose.connect('mongodb://localhost/chat', (err) => {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log('Connected to mongodb!');
+    }
 });
 const chatSchema = mongoose.Schema({
-    user : String,
-    room : String,
+    user: String,
+    room: String,
     online: String,
-    created : {type : Date,default:Date.now}
+    created: { type: Date, default: Date.now }
 });
 
-const Chat = mongoose.model('Message',chatSchema);
-// const on = mongoose.model('online',chatSchema);
+const Chat = mongoose.model('Message', chatSchema);
+const on = mongoose.model('online', chatSchema);
 // SOCKETİO
 const io = socketio.listen(server);
-io.on('connection',(socket)=>{
- 
-    
+io.on('connection', (socket) => {
+
+
     socket.on('joinRoom', (data) => {
-        
-        Chat.find({ room: { $eq: data.roomName }}, { '_id': 0, 'user' :1,'msg':1}, (error, doc) => {
-                
+
+        Chat.find({ room: { $eq: data.roomName } }, { '_id': 0, 'user': 1, 'msg': 1 }, (error, doc) => {
+            if (Chat.find({ room: { $exists: true, $not: { $size: 0 } } })) {
+                console.log("There isn't old message yet.")
+            }
+            else {
                 file = JSON.stringify(doc);
-                s = file.replace(/[\[\]{}"",]+/g, '');                
-                un=s.replace(/user:/g," ");
+                s = file.replace(/[\[\]{}"",]+/g, '');
+                un = s.replace(/user:/g, " ");
                 console.log(un)
-                socket.emit('oldMessages',un);
-                
+                socket.emit('oldMessages', un);
+            }
+
         });
-        
-        socket.join(data.roomName,() => {
-         
-            io.to(data.roomName).emit('newJoin' ,{count : getOnlineCount(io,data),message: `${data.userName}  joined to ${data.roomName} room <br> `});
-            // const onl = new on({user:`${data.userName}`,room:`${data.roomName}`});
-            // onl.save();
-            // on.find({ room: { $eq: data.roomName }},{'user':1,'_id': 0,}, (error, durum) => {
-                
-            // AA = JSON.stringify(durum);
-            // aa = AA.replace(/[\[\]{}"",]+/g, '');
+
+        socket.join(data.roomName, () => {
             socket.emit('joinedRoom');
-            // });
-      
-            
+            io.to(data.roomName).emit('newJoin', { count: getOnlineCount(io, data), message: `${data.userName}  <span class="online"></span> <br> ` });
+            const onl = new on({ user: `${data.userName}`, room: `${data.roomName}` });
+            onl.save();
+            on.find({ room: { $eq: data.roomName } }, { 'user': 1, '_id': 0, }, (error, durum) => {
+             
+                    AA = JSON.stringify(durum);
+                    aa = AA.replace(/[\[\]{}"",]+/g, '');
+                    un = aa.replace(/user:/g, " ");
+                    socket.emit('joinedRoomUser', un);
+                             
+            });
+           
+
+
             // const rooms = Object.keys(socket.rooms);
             // console.log(rooms);
-           
-         
+
+
         });
     });
-    socket.on('sendMessage',(data)=>{
-        const newMsg = new Chat({user:` ${data.userName}</strong> => ${data.message} <br>  `,room:data.roomName});
-        newMsg.save((err)=>{
-            if(err) throw err ;
-            io.to(data.roomName).emit('message',{message: `<div style="background-color:white;height:20%;border:3px solid gray; border-radius: 25px;"> <strong>${data.userName}</strong> <br>${data.message} </div> `});
+    socket.on('sendMessage', (data) => {
+        const newMsg = new Chat({ user: ` ${data.userName}</strong> => ${data.message} <br>  `, room: data.roomName });
+        newMsg.save((err) => {
+            if (err) throw err;
+            io.to(data.roomName).emit('message', { message: `<div style="background-color:white;height:20%;border:3px solid gray; border-radius: 25px;"> <strong>${data.userName}</strong> <br>${data.message} </div> ` });
         });
-      
-          
+
+
     });
-    socket.on('leave',(data)=>{
-        socket.leave(data.roomName,()=>{
+    socket.on('leave', (data) => {
+        socket.leave(data.roomName, () => {
             socket.emit('youleaved');
-            // on.deleteOne({ user: { $eq: data.userName}}).then(function(){ 
-            //     console.log("user deleted"); // Success 
-            // }).catch(function(error){ 
-            //     console.log(error); // Failure 
-            // }); 
-            io.to(data.roomName).emit('leaved' ,{count : getOnlineCount(io,data),message :`  ${data.userName}  leaved to ${data.roomName} room <br> `});
+            on.deleteOne({ user: { $eq: data.userName}}).then(function(){ 
+                console.log("user deleted"); // Success 
+            }).catch(function(error){ 
+                console.log(error); // Failure 
+            }); 
+            io.to(data.roomName).emit('leaved', { count: getOnlineCount(io, data), message: `  ${data.userName}  leaved to ${data.roomName} room <br> ` });
         });
     });
 
 });
-const getOnlineCount = (io,data) => {
-    const room = io.sockets.adapter.rooms[data.roomName] ;
+const getOnlineCount = (io, data) => {
+    const room = io.sockets.adapter.rooms[data.roomName];
     return room ? room.length : 0;
 };
